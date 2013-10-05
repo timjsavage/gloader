4dae831613e50100ac1a2a6a7a1e37992d56e96f
Benedict Dodd <ben@armakuni.com>
2013-10-05 21:43:46 +0100

    Updated config to allow config to be persisted

--- lib/gloader/config.rb
+++ lib/gloader/config.rb
@@ -6,29 +6,37 @@ module GLoader
   class Config
 
     def initialize
-      parse_config
+      parse_config(:default)
+      parse_config(:state)
     end
 
-    def config(config = {})
+    def config(type, config = {}, update = false)
       @config ||= {}
-      @config.merge!(config)
+      @config[type] ||= {}
+      @config[type].merge!(config)
+      update_config(type, config) if update
+      @config[type]
     end
 
-    def parse_config
-      config(Psych.load_file(config_path)) if File.exists?(config_path)
-      config
+    def parse_config(type)
+      config(type, Psych.load_file(config_path(type)), false) if File.exists?(config_path(type))
+      config(type)
+    end
+
+    def update_config(type, config)
+      File.open(config_path(type), 'w+') { |f| f.write(config.to_yaml) }
     end
 
     def save_default_config
-      File.open(config_path, 'w+') { |f| f.write(default_config.to_yaml) }
+      File.open(config_path(:default), 'w+') { |f| f.write(default_config.to_yaml) }
     end
 
     def default_config
       { 'platform_id' => 'default' }
     end
 
-    def config_path
-      path = File.expand_path '.gloader.yml'
+    def config_path(type)
+      path = File.expand_path ".gloader-#{type.to_s}.yml"
       path.sub!('.yml', '-test.yml') if ENV['GEM_ENV'] == 'test'
       path
     end
--- spec/unit/gloader/config_spec.rb
+++ spec/unit/gloader/config_spec.rb
@@ -8,32 +8,119 @@ describe GLoader do
 
     let(:gloader) { GLoader::Config.new }
 
-    describe '#parse_config' do
-      it 'should return an empy hash if no config file exists' do
-        gloader.parse_config.must_equal Hash.new
+    before(:each) do
+      default_path = File.expand_path('spec/unit/fixtures/config_gloader_default.yml')
+      state_path = File.expand_path('spec/unit/fixtures/config_gloader_state.yml')
+      unless gloader.config_path(:default) == default_path
+        File.delete gloader.config_path(:default) if File.exists?(gloader.config_path(:default))
       end
-      it 'should return an hash if a config exists' do
+      unless gloader.config_path(:state) == state_path
+        File.delete gloader.config_path(:state) if File.exists?(gloader.config_path(:state))
+      end
+    end
+
+    after(:each) do
+      default_path = File.expand_path('spec/unit/fixtures/config_gloader_default.yml')
+      state_path = File.expand_path('spec/unit/fixtures/config_gloader_state.yml')
+      unless gloader.config_path(:default) == default_path
+        File.delete gloader.config_path(:default) if File.exists?(gloader.config_path(:default))
+      end
+      unless gloader.config_path(:state) == state_path
+        File.delete gloader.config_path(:state) if File.exists?(gloader.config_path(:state))
+      end
+    end
+
+    describe '#initialize' do
+      it 'should return an empy hash if no config file exists' do
+        gloader.config(:default).must_equal Hash.new
+        gloader.config(:state).must_equal Hash.new
+      end
+      it 'should return a hash if default config file exists' do
+        skip # need to work out how to override the config_path before initialing
+      end
+      it 'should return a hash if state config file exists' do
+        skip # need to work out how to override the config_path before initialing
+      end
+    end
+
+    describe '#config' do
+      it 'should return an empy hash if no config file exists' do
+        gloader.config(:default).must_equal Hash.new
+        gloader.config(:state).must_equal Hash.new
+      end
+      it 'should allow the default config to be updated' do
+        FileUtils.cp File.expand_path('spec/unit/fixtures/config_gloader_default.yml'),
+                     File.expand_path('spec/unit/fixtures/config_gloader_default_temp.yml')
         gloader.singleton_class.class_eval do
-          def config_path
-            File.expand_path 'spec/unit/fixtures/config_gloader.yml'
+          def config_path(type)
+            File.expand_path 'spec/unit/fixtures/config_gloader_default_temp.yml'
           end
         end
-        config_hash = { 'platform_id' => 'default' }
-        gloader.parse_config.must_equal config_hash
+        default = { 'platform_id' => 'default' }
+        default_new = { 'platform_id' => 'default', 'iaas' => 'aws' }
+        gloader.parse_config(:default)
+        gloader.config(:default).must_equal default
+        gloader.config(:default, default_new, true).must_equal default_new
+        gloader.config(:default).must_equal default_new
+      end
+      it 'should allow the state config to be updated' do
+        FileUtils.cp File.expand_path('spec/unit/fixtures/config_gloader_state.yml'),
+                     File.expand_path('spec/unit/fixtures/config_gloader_state_temp.yml')
+        gloader.singleton_class.class_eval do
+          def config_path(type)
+            File.expand_path 'spec/unit/fixtures/config_gloader_state_temp.yml'
+          end
+        end
+        state     = { 'platform_id' => 'megaload-us',
+                      'test' => {
+                        'current' => 'load_test_2013_10_04_14_28_55_UTC_Friday'
+                      }
+                    }
+        state_new = { 'platform_id' => 'megaload-us',
+                      'test' => {
+                        'current' => 'load_test_2013_10_04_17_00_00_UTC_Friday'
+                      }
+                    }
+        gloader.parse_config(:state)
+        gloader.config(:state).must_equal state
+        gloader.config(:state, state_new, true).must_equal state_new
+        gloader.config(:state).must_equal state_new
+      end
+    end
+
+    describe '#parse_config' do
+      it 'should return an empy hash if no config file exists' do
+        gloader.parse_config(:default).must_equal Hash.new
+        gloader.parse_config(:state).must_equal Hash.new
+      end
+      it 'should return an hash if a default config exists' do
+        gloader.singleton_class.class_eval do
+          def config_path(type)
+            File.expand_path "spec/unit/fixtures/config_gloader_#{type.to_s}.yml"
+          end
+        end
+        default = { 'platform_id' => 'default' }
+        state   = { 'platform_id' => 'megaload-us',
+                    'test' => {
+                      'current' => 'load_test_2013_10_04_14_28_55_UTC_Friday'
+                    }
+                  }
+        gloader.parse_config(:default).must_equal default
+        gloader.parse_config(:state).must_equal state
       end
     end
 
     describe '#save_default_config' do
       it 'should save the default config to disk' do
         gloader.singleton_class.class_eval do
-          def config_path
-            File.expand_path 'spec/unit/fixtures/config_gloader_temp.yml'
+          def config_path(type)
+            File.expand_path "spec/unit/fixtures/config_gloader_#{type.to_s}_temp.yml"
           end
         end
         config_hash = { 'platform_id' => 'default' }
         gloader.save_default_config
-        YAML.load_file(gloader.config_path).must_equal config_hash
-        File.delete gloader.config_path
+        YAML.load_file(gloader.config_path(:default)).must_equal config_hash
+        File.delete gloader.config_path(:default)
       end
     end
   end
