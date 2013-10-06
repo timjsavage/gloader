815598c659d722c86e5bcd82d802c1699802bae5
Benedict Dodd <ben@armakuni.com>
2013-10-06 23:21:18 +0100

    Added Fog mock to actual code to allow dev

--- lib/gloader.rb
+++ lib/gloader.rb
@@ -4,3 +4,7 @@ require_relative 'gloader/core'
 require_relative 'gloader/cli'
 require_relative 'gloader/test_run'
 require_relative 'gloader/platform'
+
+# REMOVE after dev!
+Fog.mock!
+Fog::Mock.delay = 0
